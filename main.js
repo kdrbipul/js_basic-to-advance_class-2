13ff71b86533e3ea395513cf6fedef357e3d18c6
kdrbipul <kdrbipul@gmail.com>
2023-08-16 01:08:29 +0600

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // alert();
 let winingNumber = 19;
 let usserGuess = prompt("Guess a Number");
-console.log(usserGuess);
+// console.log(usserGuess);
+
+if(usserGuess===winingNumber){
+    console.log("Your Guess is Write");
+}else{
+    if(usserGuess<winingNumber){
+        console.log("too low");
+    }else{
+        console.log("too high");
+    }
+}
