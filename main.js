cc8ecde3a081afb796f528483a9c7250427c83fa
kdrbipul <kdrbipul@gmail.com>
2023-08-16 01:24:35 +0600

    first commit

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ if(usserGuess===winingNumber){
         console.log("too high");
     }
 }
+
+
+let tempInDegree = 14;
+if(tempInDegree<0){
+    console.log("extremely cold outside");
+}else if(tempInDegree<16){
+    console.log("it is cold outside");
+}else if(tempInDegree<25){
+    console.log("weather is okay");
+}else if(tempInDegree<35){
+    console.log("let's go for swim");
+}else if(tempInDegree<45){
+    console.log("weather is too hot");
+}else{
+    console.log("too hot!!");
+}
+
+console.log("Hello");
